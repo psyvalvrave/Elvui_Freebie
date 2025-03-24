e826b7935148a3874dcefea598976b094732d787
Zhecheng Li <35428744+psyvalvrave@users.noreply.github.com>
2025-03-23 23:03:59 -0700

    Change the app style to be modern fit. Hide the default windows frame and add my own frame.
    By adding my own frame, make sure 3 buttons(Maximize, Minimize, and Close button) work as before. Also add mouse button listen so user still can drag and move the UI around.

--- Elvui_Freebie.py
+++ Elvui_Freebie.py
@@ -78,6 +78,7 @@ class UpdateChecker(QThread):
 class App(QWidget):
     def __init__(self, config):
         super().__init__()
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.Window)
         self.config = config
         self.online_version = None  
         self.is_newer = False
@@ -101,16 +102,48 @@ class App(QWidget):
         self.checker.start()
 
     def initUI(self):
-        self.setWindowTitle('Elvui Freebie')
-
+        #Set Up UI
         mainLayout = QVBoxLayout()
         sourceLayout = QHBoxLayout()
         outputLayout = QHBoxLayout()
         versionLayout = QHBoxLayout()
+        
+        #title area, replace with original windows title
+        self.titleBar = QWidget(self)
+        self.titleBar.setObjectName("titleBar")
+        
+        titleBarLayout = QHBoxLayout(self.titleBar)
+        titleBarLayout.setContentsMargins(0, 0, 0, 0)
+        
+        self.titleLabel = QLabel("Elvui Freebie", self.titleBar)
+        titleBarLayout.addWidget(self.titleLabel)
+        
+        titleBarLayout.addStretch(1)
+        
+        # Minimize Button
+        self.btnMin = QPushButton("-", self.titleBar)
+        self.btnMin.setObjectName("btnMinimize")
+        self.btnMin.clicked.connect(self.showMinimized)
+        titleBarLayout.addWidget(self.btnMin)
+
+        # Maximize/Restore Button
+        self.btnMax = QPushButton("⬜", self.titleBar)
+        self.btnMax.setObjectName("btnMaximize")
+        self.btnMax.clicked.connect(self.toggleMaxRestore)
+        titleBarLayout.addWidget(self.btnMax)
+
+        # Close Button
+        self.btnClose = QPushButton("X", self.titleBar)
+        self.btnClose.setObjectName("btnClose")
+        self.btnClose.clicked.connect(self.close)
+        titleBarLayout.addWidget(self.btnClose)
+
+        mainLayout.addWidget(self.titleBar)
 
         self.updateLabel = QLabel("")
         mainLayout.addWidget(self.updateLabel)
 
+        #zip file source area
         self.pathEntry = QLineEdit()
         browseSourceBtn = QPushButton('Browse Source')
         browseSourceBtn.clicked.connect(self.browse_directory)
@@ -119,6 +152,7 @@ class App(QWidget):
         sourceLayout.addWidget(self.pathEntry)
         sourceLayout.addWidget(browseSourceBtn)
 
+        #unzip target location area
         self.outputPathEntry = QLineEdit()
         browseOutputBtn = QPushButton('Browse Output')
         browseOutputBtn.clicked.connect(self.browse_output_directory)
@@ -126,7 +160,8 @@ class App(QWidget):
         outputLayout.addWidget(QLabel('Select Output Directory:'))
         outputLayout.addWidget(self.outputPathEntry)
         outputLayout.addWidget(browseOutputBtn)
-
+        
+        #last version info area
         self.versionLabel = QLabel("Last extracted version: None")
         versionLayout.addWidget(self.versionLabel)
 
@@ -149,11 +184,15 @@ class App(QWidget):
         mainLayout.addLayout(outputLayout)
         mainLayout.addLayout(versionLayout)
         mainLayout.addWidget(extractBtn)
+        self.btnMin.setFixedSize(30, 30)
+        self.btnMax.setFixedSize(30, 30)
+        self.btnClose.setFixedSize(30, 30)
 
         self.setLayout(mainLayout)
         self.setGeometry(300, 300, 600, 400)
 
     def browse_directory(self):
+        """Check the last source directory"""
         directory = QFileDialog.getExistingDirectory(self, 'Select Directory', self.pathEntry.text())
         if directory:
             self.config['last_directory'] = directory
@@ -162,6 +201,7 @@ class App(QWidget):
             self.scan_directory(directory)
 
     def browse_output_directory(self):
+        """Check the last output directory"""
         directory = QFileDialog.getExistingDirectory(self, 'Select Directory', self.outputPathEntry.text())
         if directory:
             self.config['last_extraction_path'] = directory
@@ -169,6 +209,7 @@ class App(QWidget):
             self.outputPathEntry.setText(directory)
 
     def scan_directory(self, directory):
+        """Scan the downloading directory to find elvui related zip file"""
         if directory:
             files = os.listdir(directory)
             zip_files = [file for file in files if file.endswith('.zip') and file.lower().startswith('elvui')]
@@ -176,6 +217,7 @@ class App(QWidget):
             self.fileList.addItems(zip_files)
 
     def extract_zip(self):
+        """Function for extracting zip files"""
         selected_items = self.fileList.selectedItems()
         input_directory = self.pathEntry.text()
         output_directory = self.outputPathEntry.text()
@@ -254,6 +296,7 @@ class App(QWidget):
             QMessageBox.warning(self, "Error", f"An error occurred during download: {e}")
             
     def onUpdateChecked(self, online_ver):
+        """Check is new update available on official website"""
         self.online_version = online_ver
         last_local = self.config.get('last_extracted_version', '0.0')
         if online_ver == "":
@@ -305,17 +348,77 @@ class App(QWidget):
                     )
                 else:
                     self.updateLabel.setText("You have the latest version of ElvUI installed.")
+                    
+    def apply_styles(app):
+        """Style modification"""
+        app.setStyle("Fusion")
+        style_sheet = """
+
+            QWidget {
+                background-color: #2b2b2b;
+                color: #ffffff;
+                font-family: Arial, sans-serif;
+            }
+            QPushButton {
+                background-color: #3c3f41;
+                border: 1px solid #555;
+                padding: 5px;
+                border-radius: 4px;
+            }
+            QPushButton:hover {
+                background-color: #4e5254;
+            }
+            QLineEdit {
+                background-color: #3c3f41;
+                border: 1px solid #555;
+                border-radius: 4px;
+                padding: 4px;
+            }
+            QListWidget {
+                background-color: #3c3f41;
+                border: 1px solid #555;
+                border-radius: 4px;
+            }
+            QLabel {
+                font-size: 14px;
+            }
+        """
+        app.setStyleSheet(style_sheet)
             
-        
+    def toggleMaxRestore(self):
+        """Toggles between normal size and maximized."""
+        if self.isMaximized():
+            self.showNormal()
+            self.btnMax.setText("⬜")
+        else:
+            self.showMaximized()
+            self.btnMax.setText("❐")
             
+    def mousePressEvent(self, event):
+        """Remember the position of the mouse for window dragging."""
+        if event.button() == Qt.LeftButton:
+            if event.pos().y() <= self.titleBar.height():
+                self.dragPos = event.globalPos() - self.frameGeometry().topLeft()
+                event.accept()
+
+    def mouseMoveEvent(self, event):
+        """Move the window as the mouse moves."""
+        if event.buttons() == Qt.LeftButton and self.dragPos is not None:
+            self.move(event.globalPos() - self.dragPos)
+            event.accept()
+
+    def mouseReleaseEvent(self, event):
+        """Reset drag position on mouse release."""
+        if event.button() == Qt.LeftButton:
+            self.dragPos = None
 
     
 
 
 def main():
     config = load_config()
-
     app = QApplication(sys.argv)
+    App.apply_styles(app)
     ex = App(config)
     ex.show()
     sys.exit(app.exec_())
